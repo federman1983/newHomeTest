b17d71fcedda0fb1ea96fe57457898d5546dc78f
federman1983 <federman.sergey@gmail.com>
2025-04-08 21:10:28 +0300

    Hello Federman Sergushka

--- tempFile.py
+++ tempFile.py
@@ -1,10 +1,12 @@
 class Table:
     def display_table():
         print("You are display this table to me ")
-
+    def add_item_to_table(name,cost):
+        print("This blok add the item to the table")
 
 
 
 
 Table.display_table()
 print("Hello World to git")
+Table.add_item_to_table("Sergeyu",1253)
